428cbdead95d4c3e9402448fd5df86b33cbad97f
Noah Bauer <bauer.noah712@gmx.de>
2023-02-07 23:18:46 +0100

    added additional needed fields for tennis match

--- src/model/tennis/tennis_match.rs
+++ src/model/tennis/tennis_match.rs
@@ -1,9 +1,9 @@
-use std::fmt::Display;
-
-use async_graphql::{InputObject, SimpleObject, ID};
+use async_graphql::{Enum, InputObject, SimpleObject, ID};
+use strum::Display;
 
 use super::{
     match_settings::{InputTennisMatchSettings, TennisMatchSettings},
+    player::{InputTennisPlayer, TennisPlayer},
     score::{InputTennisScoreData, TennisScoreData},
 };
 use crate::shared::input_to_simple_object_converter::InputToSimpleObjectConvertible;
@@ -13,12 +13,32 @@ pub struct TennisMatch {
     pub id: ID,
     pub score_stack: Vec<TennisScoreData>,
     pub match_settings: TennisMatchSettings,
+    pub serving_starter_home: Vec<Option<TennisPlayer>>,
+    pub serving_starter_guest: Vec<Option<TennisPlayer>>,
+    pub starting_time: String,
+    pub starting_team: OpponentType,
 }
 
-impl Display for TennisMatch {
+impl std::fmt::Display for TennisMatch {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{:?}, {}", self.score_stack, self.match_settings)
+        write!(
+            f,
+            "{}, {:?}, {}, {:?}, {:?}, {}, {}",
+            self.id.to_string(),
+            self.score_stack,
+            self.match_settings,
+            self.serving_starter_home,
+            self.serving_starter_guest,
+            self.starting_time,
+            self.starting_team
+        )
     }
+}
+
+#[derive(Clone, Copy, Debug, Display, Enum, Eq, PartialEq)]
+pub enum OpponentType {
+    Home,
+    Guest,
 }
 
 #[derive(InputObject)]
@@ -26,6 +46,10 @@ pub struct InputTennisMatch {
     pub id: ID,
     pub score_stack: Vec<InputTennisScoreData>,
     pub match_settings: InputTennisMatchSettings,
+    pub serving_starter_home: Vec<Option<InputTennisPlayer>>,
+    pub serving_starter_guest: Vec<Option<InputTennisPlayer>>,
+    pub starting_time: String,
+    pub starting_team: OpponentType,
 }
 
 impl InputToSimpleObjectConvertible<TennisMatch> for InputTennisMatch {
@@ -38,6 +62,24 @@ impl InputToSimpleObjectConvertible<TennisMatch> for InputTennisMatch {
                 .map(|score_data| score_data.to_simple_object())
                 .collect(),
             match_settings: self.match_settings.to_simple_object(),
+            serving_starter_home: self
+                .serving_starter_home
+                .iter()
+                .map(|input_player| match input_player {
+                    Some(player) => Some(player.to_simple_object()),
+                    None => None,
+                })
+                .collect(),
+            serving_starter_guest: self
+                .serving_starter_guest
+                .iter()
+                .map(|input_player| match input_player {
+                    Some(player) => Some(player.to_simple_object()),
+                    None => None,
+                })
+                .collect(),
+            starting_time: self.starting_time.to_string(),
+            starting_team: self.starting_team,
         }
     }
 }
