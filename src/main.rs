5c385efd6d9789b0217d27c1c6a07e4f976f59b8
Christian Krämer <inf4169@hs-worms.de>
2023-02-10 11:00:34 +0100

    Changed lock file to correct import of api

--- src/main.rs
+++ src/main.rs
@@ -21,7 +21,7 @@ async fn main() -> std::io::Result<()> {
         .data(Storage::default())
         .finish();
 
-    print!("{}", &schema.sdl());
+    // print!("{}", &schema.sdl());
     HttpServer::new(move || {
         let logger = Logger::default();
         App::new()
